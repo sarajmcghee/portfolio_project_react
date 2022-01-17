af2621d032ee4ef4774e7cf5f19a2dc37b79da52
Sara McGhee <sarajmcghee@gmail.com>
2022-01-17 08:01:35 -0500

    Initial McGhees Eatery

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
+import { MENUITEMS } from './shared/menuitem';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+        menuitems: MENUITEMS
+    };
+}
     render() {
         return (
             <div className="App">
@@ -11,6 +19,7 @@ class App extends Component {
                     <NavbarBrand href="/">McGhee's Eatery</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory menuitems={this.state.menuitems} />
             </div>
         );
     }
---
+++ src/components/DirectoryComponent.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle } from 'reactstrap';
+
+class Directory extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedMenuitem: null
+        };
+    }
+
+    onMenuitemSelect(menuitem) {
+        this.setState({selectedMenuitem: menuitem});
+    }
+
+    renderSelectedMenuitem(menuitem) {
+        if (menuitem) {
+            return (
+                <Card>
+                    <CardImg top src={menuitem.image} alt={menuitem.name} />
+                    <CardBody>
+                        <CardTitle>{menuitem.name}</CardTitle>
+                        <CardText>{menuitem.description}</CardText>
+                    </CardBody>
+                </Card>
+            );
+        }
+        return <div />;
+    }
+
+    render() {
+        const directory = this.props.menuitems.map(menuitem => {
+            return (
+                <div key={menuitem.id} className="col-md-5 m-1">
+                    <Card onClick={() => this.onMenuitemSelect(menuitem)}>
+                        <CardImg width="100%" src={menuitem.image} alt={menuitem.name} />
+                        <CardImgOverlay>
+                            <CardTitle>{menuitem.name}</CardTitle>
+                        </CardImgOverlay>
+                    </Card>
+                </div>
+            );
+        });
+
+        return (
+            <div className="container">
+                <div className="row">
+                    {directory}
+                </div>
+                <div className="row">
+                    <div className="col-md-5 m-1">
+                        {this.renderSelectedMenuitem(this.state.selectedMenuitem)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Directory;
